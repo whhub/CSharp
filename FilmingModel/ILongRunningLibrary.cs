008a582af7fca427857a4f9f115fc98e98c437ce
whhub <wanghui122004@me.com>
2016-10-31 09:33:26 +0800

    [UT]Moq

--- FilmingModel/ILongRunningLibrary.cs
+++ FilmingModel/ILongRunningLibrary.cs
@@ -1,7 +1,24 @@
-﻿namespace UnitTestExample
+﻿using System.Threading;
+
+namespace UnitTestExample
 {
     public interface ILongRunningLibrary
     {
         string RunForALongTime(int interval);
+    }
+
+    public class LongRunningLibrary : ILongRunningLibrary
+    {
+
+        #region Implementation of ILongRunningLibrary
+
+        public string RunForALongTime(int interval)
+        {
+            var timeToWait = interval*1000;
+            Thread.Sleep(timeToWait);
+            return string.Format("Waited {0} seconds", interval);
+        }
+
+        #endregion
     }
 }
---
+++ FilmingModelUnitTest/MoqExamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestExample.UT
+{
+    [TestClass]
+    public class MoqExamples
+    {
+        private Mock<ILongRunningLibrary> _longRunningLibrary;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _longRunningLibrary = new Mock<ILongRunningLibrary>();
+
+            _longRunningLibrary.Setup(lrl => lrl.RunForALongTime(It.IsAny<int>()))
+                .Returns((int i) => string.Format("This method has been mocked! The input value was {0}", i));
+
+            _longRunningLibrary.Setup(lrl => lrl.RunForALongTime(0))
+                .Throws(new ArgumentException("0 is not a valid interval!"));
+        }
+
+        [TestMethod]
+        public void TestLongRunningLibrary()
+        {
+           const int interval = 1000;
+            var result = _longRunningLibrary.Object.RunForALongTime(interval);
+            Debug.WriteLine(string.Format("Return from method was '{0}'", result));
+        }
+    }
+}
