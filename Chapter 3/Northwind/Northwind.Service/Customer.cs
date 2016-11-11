d48884b12b42ff988de747b8d34b3a30a1f01c05
whhub <wanghui122004@me.com>
2016-11-11 18:00:54 +0800

    [MVVM Survival][chapter 4]Service - half

---
+++ Chapter 3/Northwind/Northwind.Service/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace Northwind.Service
+{
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public string CustomerID { get; set; }
+
+        [DataMember]
+        public string CompanyName { get; set; }
+
+        [DataMember]
+        public string ContractName { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public string Region { get; set; }
+
+        [DataMember]
+        public string PostalCode { get; set; }
+
+        [DataMember]
+        public string Country { get; set; }
+
+        [DataMember]
+        public string Phone { get; set; }
+    }
+}
---
+++ Chapter 3/Northwind/Northwind.Service/ICustomer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Northwind.Data;
+
+namespace Northwind.Service
+{
+    [ServiceContract]
+    public interface ICustomerService
+    {
+        [OperationContract]
+        IList<Customer> GetCustomers();
+
+        [OperationContract]
+        Customer GetCustomer(string customerID);
+    }
+
+    public class CustomerService : ICustomerService
+    {
+        private readonly NORTHWNDEntities _northwndEntities = new NORTHWNDEntities();
+
+        #region Implementation of ICustomerService
+
+        public IList<Customer> GetCustomers()
+        {
+            return _northwndEntities.Customers
+                .Select(
+                    c => new Customer
+                    {
+                        CustomerID = c.CustomerID,
+                        CompanyName = c.CompanyName
+                    }).ToList();
+        }
+
+        public Customer GetCustomer(string customerID)
+        {
+            var customer = _northwndEntities.Customers.Single(c => c.CustomerID == customerID);
+            return new Customer
+            {
+                CustomerID = customer.CustomerID,
+                CompanyName = customer.CompanyName,
+                ContractName = customer.ContactName,
+                Address = customer.Address,
+                City = customer.City,
+                Country = customer.Country,
+                Region = customer.Region,
+                PostalCode = customer.PostalCode,
+                Phone = customer.Phone
+            };
+        }
+
+        #endregion
+    }
+}
