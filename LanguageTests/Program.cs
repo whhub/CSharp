c0ba915f58d5829000f811ae0e9681b8f6ea7245
whhub <wanghui122004@me.com>
2017-01-18 11:46:17 +0800

    [LanguageTests] Multi-Interface Implement

---
+++ LanguageTests/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LanguageTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new C2();
+            Console.WriteLine(c.Count());
+        }
+    }
+
+
+    interface I1<T>
+    {
+        int Count();
+    }
+
+    interface I2<T> : I1<T>
+    {
+         
+    }
+
+    class C1<T> : I2<T>
+    {
+        #region Implementation of I1
+
+        public int Count()
+        {
+            return 1;
+        }
+
+        #endregion
+    }
+
+    interface I3
+    {
+        int Count();
+    }
+
+    class C2 : C1<int>, I3
+    {
+        
+    }
+}
