0293bf7443ccf70c631d54b67350c0bf801703ce
whhub <wanghui122004@me.com>
2017-09-13 17:36:51 +0800

    [Code Snippet][002] Traversal of Enum

---
+++ CodeSnippet/EnumSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EnumSample
+{
+
+        enum FooEnum
+        {
+            A = 1,
+            B = 2,
+            C = 3
+        }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            foreach (var value in Enum.GetValues(typeof(FooEnum)))
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
