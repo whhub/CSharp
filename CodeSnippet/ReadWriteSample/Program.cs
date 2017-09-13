ddfa4ad81ade4ba615388657530a12868fc8aae5
whhub <wanghui122004@me.com>
2017-09-13 14:51:29 +0800

    [Code Snippet][001] Write info to file

---
+++ CodeSnippet/ReadWriteSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ReadWriteSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "data.txt";
+            var fileStream = File.Exists(path)
+                ? File.OpenWrite(path)
+                : File.Create(path);
+
+            var stream = new StreamWriter(fileStream);
+            stream.WriteLine("a b c");
+
+            stream.Close();
+        }
+    }
+}
