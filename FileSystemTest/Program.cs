a1fd2c8a64f975e7a4d3ab981a9404f9cf8fa22a
whhub <wanghui122004@me.com>
2018-01-18 10:13:10 +0800

    [Feature Test] [0001] FileSystemTest

---
+++ FileSystemTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileSystemTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowDiskSpace();
+        }
+
+        public static void ShowDiskSpace()
+        {
+            foreach (var driveInfo in DriveInfo.GetDrives())
+            {
+                Console.WriteLine("[{0}]\t[{1}]", driveInfo.Name, driveInfo.TotalFreeSpace/(1024*1024*1024));
+            }
+        }
+
+        //public static long GetHardDiskFreeSpace(string driverName)
+        //{
+        //    long freeSpace = new long();
+        //    var sDriverPath = driverName + ":\\";
+
+        //    var driver = DriveInfo.GetDrives().FirstOrDefault(d => d.Name == sDriverPath);
+
+        //    freeSpace = driver.TotalFreeSpace / (1024 * 1024 * 1024);
+        //    return freeSpace;
+        //}
+
+    }
+}
