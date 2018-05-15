5f2dc52f99d329c93eb0118bc61ee9006246997e
whhub <wanghui122004@me.com>
2018-05-15 15:57:21 +0800

    [ExchangeToElasticsearch][001] 访问 POP3 服务器 smtp.united-imaging.com

---
+++ ExchangeToElasticsearch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using LumiSoft.Net.Mail;
+using LumiSoft.Net.POP3.Client;
+
+namespace ExchangeToElasticsearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = "softtester";
+            var pwd = "xAcmQ3gg";
+            var pop3_server = "smtp.united-imaging.com";
+            var pop3_port = 995;
+
+            using (var pop3 = new POP3_Client())
+            {
+                pop3.Connect(pop3_server, pop3_port, true);
+                pop3.Login(user, pwd);
+
+                var messages = pop3.Messages;
+                Console.WriteLine("Message Count: {0}", messages.Count);
+                Console.WriteLine("Sample Mail: ");
+
+                var sampleMessage = messages[0];
+                var messageBytes = sampleMessage.MessageToByte();
+
+                var mimeMessage = Mail_Message.ParseFromByte(messageBytes);
+                Console.WriteLine("Sender : {0}", mimeMessage.From);
+                var receiver = String.Join(";", mimeMessage.To);
+                Console.WriteLine("Receiver: {0}", receiver);
+                Console.WriteLine("Body: {0}", mimeMessage.BodyText ?? "Content is null");
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
