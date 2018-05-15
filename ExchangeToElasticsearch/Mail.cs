1b63d66bca3b529cf581d67182f84d77ad2b3aa8
whhub <wanghui122004@me.com>
2018-05-15 16:58:40 +0800

    [ExchangeToElasticsearch][005] Elasticsearch Index Mail Mapping

---
+++ ExchangeToElasticsearch/Mail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nest;
+
+namespace ExchangeToElasticsearch
+{
+    [ElasticsearchType(Name="Mail")]
+    class Mail
+    {
+        public string From { get; set; }
+        public string[] To { get; set; }
+        public string[] Cc { get; set; }
+        public string[] Bcc { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string[] Attachments { get; set; }
+        public DateTime SentTime { get; set; }
+        public bool HasSentOut { get; set; }
+        public bool HasAttachment { get; set; }
+    }
+}
