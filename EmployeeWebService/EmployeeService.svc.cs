64b85c957e0c076587b13ebff0a0aed553b7fc85
whhub <wanghui122004@me.com>
2018-06-06 09:30:33 +0800

    EmployeeService GetSubordinates

--- EmployeeWebService/EmployeeService.svc.cs
+++ EmployeeWebService/EmployeeService.svc.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using Oracle.ManagedDataAccess.Client;
 
 namespace EmployeeWebService
 {
@@ -18,7 +20,48 @@ namespace EmployeeWebService
 
         public IEnumerable<string> GetSubordinates(string id)
         {
-            return new List<string>{"foo1", "foo2"};
+            string constr = @"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.6.204.13) (PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=HR92PRD)));User Id=ifpsis; Password=Asdf5623";
+
+            IList<string> subOrdinates = new List<string>();
+            Queue<string> idQueue = new Queue<string>();
+            idQueue.Enqueue(id);
+
+            var con = new OracleConnection(constr);
+            try
+            {
+                con.Open();
+                Console.WriteLine("成功连接数据库");
+                while (idQueue.Count>0)
+                {
+                    var currentId = idQueue.Dequeue();
+                    var com = con.CreateCommand();
+                    var sql = string.Format("select OPRID from PS_UIH_IFIS_VW WHERE OPRID2 = '{0}'",currentId);
+                    Console.WriteLine(sql);
+                    com.CommandText = sql;
+                    var oracleDataReader = com.ExecuteReader();
+                    while (oracleDataReader.Read())
+                    {
+                        var subordinateId = oracleDataReader.GetString(0);
+                        Console.Write(subordinateId);
+                        if (!subOrdinates.Contains(subordinateId))
+                        {
+                            Console.WriteLine(" found");
+                            subOrdinates.Add(subordinateId);
+                            idQueue.Enqueue(subordinateId);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+            return subOrdinates;
         }
     }
 }
