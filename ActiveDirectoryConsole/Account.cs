425c4743b350e49b16dd85cac80e6451bd775e86
whhub <wanghui122004@me.com>
2018-06-21 10:55:45 +0800

    [ActiveDirectoryConsole] TraverseGroupMember to Fill Acount Permission

--- ActiveDirectoryConsole/Account.cs
+++ ActiveDirectoryConsole/Account.cs
@@ -9,6 +9,7 @@ namespace ActiveDirectoryConsole
         public string Name { get; set; }
         public string Description { get; set; }
         public string Department { get; set; }
+        public string DistinDistinguishedName { get; set; }
 
         public IList<Group> InternetAccessPermissions { get; } = new List<Group>();
         public IList<Group> VpnPermissions { get; } = new List<Group>();
--- ActiveDirectoryConsole/Program.cs
+++ ActiveDirectoryConsole/Program.cs
@@ -17,12 +17,18 @@ namespace ActiveDirectoryConsole
             Console.ReadKey();
         }
 
-        private readonly IList<Account> _accounts = new List<Account>();
+        private static readonly IList<Account> _accounts = new List<Account>();
         private static readonly IList<Group> _groups = new List<Group>();
         private const string InternetAccessPermissionGroupRegex = "WG_*";
         private const string VpnPermissionGroupRegex = "SVN-*";
         private const string MailboxPermissionGroup = "AuthorizedMailbox";
         private const string UsbPermissionGroup = "EnabledUSB_R&W";
+
+        private const string LDAP_CN = "cn";
+        private const string LDAP_Description = "Description";
+        private const string LDAP_DistinguishedName = "distinguishedName";
+        private const string LDAP_Member = "member";
+        private const string LDAP_AcountName = "sAMAccountName";
 
 
         private const string LdapOuUsersOuUnitedImagingDcUnitedImagingDcCom = "LDAP://OU=Users,OU=United_Imaging,DC=united-imaging,DC=com";
@@ -56,17 +62,23 @@ namespace ActiveDirectoryConsole
 
         private static void TraverseGroupMember(DirectoryEntry groupEntry)
         {
-            var groupName = groupEntry.Properties["cn"][0] as string;
-            var groupDescription = groupEntry.Properties["Description"][0] as string;
-            var componentName = groupEntry.Properties["distinguishedName"][0] as string;
+            var groupName = groupEntry.Properties[LDAP_CN][0] as string;
+            var groupDescription = groupEntry.Properties[LDAP_Description][0] as string;
+            var componentName = groupEntry.Properties[LDAP_DistinguishedName][0] as string;
             _groups.Add(new Group {Name = groupName, Description = groupDescription, DistinguishedName = componentName});
 
-            foreach (var member in groupEntry.Properties["member"])
+            foreach (var member in groupEntry.Properties[LDAP_Member])
             {
                 var memberPath = $"LDAP://{member}";
                 using (var memberEntry = new DirectoryEntry(memberPath))
                 {
                     ListEntryProperties(memberEntry);
+                    var memberDistinguishedName = memberEntry.Properties[LDAP_DistinguishedName][0] as string;
+                    var account = _accounts.FirstOrDefault(a => a.DistinDistinguishedName == memberDistinguishedName);
+                    account = account ?? new Account {DistinDistinguishedName = memberDistinguishedName};
+                    var descriptionCollection = memberEntry.Properties[LDAP_Description];
+                    account.Description = descriptionCollection.Count>0 ? descriptionCollection[0] as string : string.Empty;
+                    account.Name = memberEntry.Properties[LDAP_AcountName][0] as string;
                 }
 
             }
