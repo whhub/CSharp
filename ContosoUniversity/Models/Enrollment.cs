761697a2517130873e568437a74b3a15de75d378
whhub <wanghui122004@me.com>
2018-06-24 14:57:21 +0800

    [.NET][ASP.NET][MVC][EntityFramework][012] Foreign object should be virtual

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -12,7 +12,7 @@
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
 
-        public Student Student { get; set; }
-        public Course Course { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Course Course { get; set; }
     }
 }
